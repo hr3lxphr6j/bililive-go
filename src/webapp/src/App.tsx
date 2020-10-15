27cb96d2cb734b5512c3cf296ee85dd4cda377c5
daofeng2015 <jcb20150301@gmail.com>
2020-10-15 18:06:25 +0800

    add:增加中文支持

--- src/webapp/src/App.tsx
+++ src/webapp/src/App.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import { Switch, Route } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN'
 import RootLayout from './component/layout/index';
 import LiveList from './component/live-list/index';
 import LiveInfo from './component/live-info/index';
 
 const App: React.FC = () => {
   return (
-    <RootLayout>
-      <Switch>
-        <Route path="/liveInfo" component={LiveInfo}></Route>
-        <Route path="/" component={LiveList}></Route>
-      </Switch>
-    </RootLayout>
+    <ConfigProvider locale={zhCN}>
+      <RootLayout>
+        <Switch>
+          <Route path="/liveInfo" component={LiveInfo}></Route>
+          <Route path="/" component={LiveList}></Route>
+        </Switch>
+      </RootLayout>
+    </ConfigProvider>
+
   );
 }
 
