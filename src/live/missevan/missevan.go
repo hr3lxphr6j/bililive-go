dab9330c27d9a70c35be5b131ec5f1778561dd7e
DUYA112233 <33391055+DUYA112233@users.noreply.github.com>
2023-05-05 08:05:13 +0900

    Update missevan.go

--- src/live/missevan/missevan.go
+++ src/live/missevan/missevan.go
@@ -17,7 +17,7 @@ const (
 	domain = "fm.missevan.com"
 	cnName = "猫耳"
 
-	roomInitUrl  = "https://fm.missevan.com/api/v2/live/"
+	roomInitUrl = "https://fm.missevan.com/api/v2/live/"
 )
 
 func init() {
@@ -36,56 +36,57 @@ type Live struct {
 	internal.BaseLive
 }
 
-func (l *Live) getRoomId() (string,error) {
+func (l *Live) getRoomId() (string, error) {
 	paths := strings.Split(l.Url.Path, "/")
 	if len(paths) < 2 {
-		return "",live.ErrRoomUrlIncorrect
+		return "", live.ErrRoomUrlIncorrect
 	}
 	roomid := paths[2]
 
-	return roomid,nil
+	return roomid, nil
 }
 
-func (l *Live) getRoomInfo() ([]byte,error) {
-	roomid,err := l.getRoomId()
-	if err!=nil{
-		return nil,err
-	}
-	resp, err := requests.Get(roomInitUrl+roomid)
+func (l *Live) getRoomInfo() ([]byte, error) {
+	roomid, err := l.getRoomId()
 	if err != nil {
-		return nil,err
+		return nil, err
+	}
+	resp, err := requests.Get(roomInitUrl + roomid)
+	if err != nil {
+		return nil, err
 	}
 	if resp.StatusCode != http.StatusOK {
-		return nil,live.ErrRoomNotExist
+		return nil, live.ErrRoomNotExist
 	}
 	body, err := resp.Bytes()
 	if err != nil || gjson.GetBytes(body, "code").Int() != 0 {
-		return nil,live.ErrRoomNotExist
+		return nil, live.ErrRoomNotExist
 	}
-	return body,nil
+	return body, nil
 }
 
 func (l *Live) GetInfo() (info *live.Info, err error) {
-	body,err := l.getRoomInfo()
-	if err !=nil{
-		return nil,live.ErrRoomNotExist
+	body, err := l.getRoomInfo()
+	if err != nil {
+		return nil, live.ErrRoomNotExist
 	}
 	info = &live.Info{
-		Live:     l,
-		HostName: gjson.GetBytes(body, "info.room.creator_username").String(),
-		RoomName: gjson.GetBytes(body, "info.room.name").String(),
-		Status:   gjson.GetBytes(body, "info.room.status.open").Int() == 1,
+		Live:      l,
+		HostName:  gjson.GetBytes(body, "info.room.creator_username").String(),
+		RoomName:  gjson.GetBytes(body, "info.room.name").String(),
+		Status:    gjson.GetBytes(body, "info.room.status.open").Int() == 1,
+		AudioOnly: true,
 	}
 
-	return info,nil
+	return info, nil
 }
 
 func (l *Live) GetStreamUrls() (us []*url.URL, err error) {
-	body,err := l.getRoomInfo()
-	if err !=nil{
-		return nil,live.ErrRoomNotExist
+	body, err := l.getRoomInfo()
+	if err != nil {
+		return nil, live.ErrRoomNotExist
 	}
-	return utils.GenUrls(gjson.GetBytes(body,"info.room.channel.flv_pull_url").String())
+	return utils.GenUrls(gjson.GetBytes(body, "info.room.channel.flv_pull_url").String())
 }
 
 func (l *Live) GetPlatformCNName() string {
