6b482476cb55a9d228bf98fdac1fd08e74c832f7
WLaoDuo <69497874+WLaoDuo@users.noreply.github.com>
2024-08-06 19:22:59 +0800

    win

--- src/hack/release.sh
+++ src/hack/release.sh
@@ -42,7 +42,7 @@ for dist in $(go tool dist list); do
   platform=$(echo ${dist} | cut -d'/' -f1)
   arch=$(echo ${dist} | cut -d'/' -f2)
   echo PLATFORM=${platform} ARCH=${arch}
-  if [ $dist == "windows/amd64" ];then
+  if [[ ${platform} == windows ]]; then
     echo "build "$dist
     make PLATFORM=${platform} ARCH=${arch} bililive
   fi
